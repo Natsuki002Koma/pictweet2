868912755681cee31b41eae793ca3d568797129d
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-11-02 12:38:23 +0900

    Tweetsテーブルのnameカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_023428) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_033441) do
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "text"
